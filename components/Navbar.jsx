c5a293daac1a338141df11afae60bc14ee9de7fe
sami <samuelyeshambel9@gmail.com>
2024-03-24 12:07:54 +0300

    add buttons

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import Link from 'next/link'
+const links=[
+  {url:'/',title:"Home"},
+  {url:'/about',title:"About"},
+  {url:'portfolio',title:"Portfolio"},
+  {url:'/contact',title:"Contact"}
+];
 const Navbar = () => {
+  const [open,setOpen]=useState(false);
   return (
     <div className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
 <div className=''>
@@ -9,7 +17,21 @@ const Navbar = () => {
   <span className='w-12 h-88 rounded bg-white text-black flex items-center justify-center'>.white</span>
  </Link>
 </div>
-<div className=''></div>
+<div className=''>
+ 
+  <button className='w-10 h-8 flex flex-col justify-between z-50 relative ' onClick={()=>setOpen(!open)}>
+    <div className='w-10 h-1 bg-white rounded'></div>
+    <div className='w-10 h-1 bg-white rounded'></div>
+    <div className='w-10 h-1 bg-white rounded'></div>
+  </button>
+{open &&
+  <div className='absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl'>
+      {links.map(link=>(
+    <Link href={link.url} key={link.title}>{link.title}</Link>
+      ))}
+  </div>
+}
+</div>
     </div>
   )
 }
