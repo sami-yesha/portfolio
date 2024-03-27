51b6f41c71bdf3ef82447b95cad7bdf79bb389ee
sami <samuelyeshambel9@gmail.com>
2024-03-27 21:02:55 +0300

    .

--- app/testanimation/page.jsx
+++ app/testanimation/page.jsx
@@ -2,12 +2,25 @@
 import React from "react";
 import { motion } from "framer-motion";
 const page = () => {
+    const variants={
+        variant1:{
+            x: 400, y:300 ,opacity:0.5 ,
+            transition:{
+                duration:3,
+            }
+        },
+        variant2: {
+           x:100,y:-100, rotation:90, 
+        },
+    };
   return (
     <div className="h-full flex items-center justify-center">
       <motion.div
         className="w-96 h-96 bg-red-400 rounded"
-        initial={{ x: -100 }}
-        animate={{ x: 100 }}
+        
+        variants={variants}
+        animate="variant1"
+        
       ></motion.div>
     </div>
   );
