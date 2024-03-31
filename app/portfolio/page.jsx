f4b7954286924aca2a5d6a7fc056b0a762bb327b
sami <samuelyeshambel9@gmail.com>
2024-03-31 16:23:42 +0300

    portfolio

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -1,10 +1,14 @@
+'use client'
 import React from 'react'
-
+import {motion} from 'framer-motion'
 const page = () => {
   return (
-    <div>
-      Portfolio
-    </div>
+    <motion.div
+    className="h-full"
+    initial={{ y: "-200vh" }}
+    animate={{ y: "0%" }}
+    transition={{ duration: 1 }}
+  >Portfolio</motion.div>
   )
 }
 
