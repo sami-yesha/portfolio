832cfb6b9aa80f3fda0485e55f8e77dc055ca3d5
sami <samuelyeshambel9@gmail.com>
2024-03-31 16:23:15 +0300

    about

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,10 +1,14 @@
+'use client'
 import React from 'react'
-
+import {motion} from 'framer-motion'
 const page = () => {
   return (
-    <div>
-      about
-    </div>
+    <motion.div
+    className="h-full"
+    initial={{ y: "-200vh" }}
+    animate={{ y: "0%" }}
+    transition={{ duration: 1 }}
+  >About</motion.div>
   )
 }
 
