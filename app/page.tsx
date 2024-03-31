e443b07a01431ef6763ea2fe74b1d056a180551d
sami <samuelyeshambel9@gmail.com>
2024-03-31 16:09:13 +0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,38 @@
+"use client";
 import Image from "next/image";
 import React from "react";
-
+import { motion } from "framer-motion";
 export default function Home() {
   return (
-  <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
-    <div className="h-1/2 relative lg:h-full lg:w-1/2" >
-  <Image src="/hero.png" alt="sami" fill className="object-contain"/>
-    </div>
-    <div className="h-1/2 flex lg:h-full lg:w-1/2 flex-col gap-8 items-center justify-center">
-      <h1 className="text-4xl font-bold md:text-6xl"> Crafting digital experiances , Designing tomorrow. </h1>
-      <p className="md:text-xl">Armed with a deep understanding of machine learning , web development and a flair for creating seamless user experiences . </p>
-      <div className="flex gap-4 w-full">
-        <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">View My Work </button>
-        <button className="p-4 rounded-lg ring-1 ring-black">Contact Me </button>
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
+        <div className="h-1/2 relative lg:h-full lg:w-1/2">
+          <Image src="/hero.png" alt="sami" fill className="object-contain" />
+        </div>
+        <div className="h-1/2 flex lg:h-full lg:w-1/2 flex-col gap-8 items-center justify-center">
+          <h1 className="text-4xl font-bold md:text-6xl">
+            {" "}
+            Crafting digital experiances , Designing tomorrow.{" "}
+          </h1>
+          <p className="md:text-xl">
+            Armed with a deep understanding of machine learning , web
+            development and a flair for creating seamless user experiences .{" "}
+          </p>
+          <div className="flex gap-4 w-full">
+            <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+              View My Work{" "}
+            </button>
+            <button className="p-4 rounded-lg ring-1 ring-black">
+              Contact Me{" "}
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
+    </motion.div>
   );
 }
