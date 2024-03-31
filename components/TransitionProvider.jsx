8b00b35bd6c65b7df9ef8cac8af45d35b9a9ac30
sami <samuelyeshambel9@gmail.com>
2024-03-31 16:09:21 +0300

    .

--- components/TransitionProvider.jsx
+++ components/TransitionProvider.jsx
@@ -7,13 +7,28 @@ import { usePathname } from "next/navigation";
 const TransitionProvider = ({ children }) => {
   const pathName=usePathname();
   return (
-    <AnimatePresence>
+    <AnimatePresence mode="wait">
       <div key={pathName} className="w-screen h-screen bg-gradient-to-b from-blue-50 to-red-100">
         <motion.div
           className="h-screen w-screen fixed bg-black rounded-b-[100px]  z-40"
           animate={{ height: "0vh" }}
-          exit={{ height: "100vh" }}
+          exit={{ height: "140vh" }}
           transition={{ duration: 0.5, ease: "easeOut" }}
+        />
+         <motion.div
+          className="fixed m-auto top-0 left-0 right-0 bottom-0 text-white text-8xl cursor-default bg-black z-50 w-fit h-fit"
+          animate={{ opacity:0}}
+          initial={{opacity:1}}
+          exit={{opacity:0}}
+          transition={{ duration: 0.5, ease: "easeOut" }}
+        >
+          {pathName.substring(1)}
+        </motion.div>
+        <motion.div
+          className="h-screen w-screen fixed bg-black rounded-t-[100px] bottom-0  z-30"
+          initial={{ height: "140vh" }}
+          animate={{ height: "0vh" , transition:{delay:0.5}}}
+          
         />
         <div className="h-24">
           {" "}
